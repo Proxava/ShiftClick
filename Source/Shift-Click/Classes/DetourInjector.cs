9a0f9f0fd2bb46686e207f7963db003b19bd9fbb
Proxava <proxava@gmail.com>
2016-12-01 18:26:57 +0100

    Upload

---
+++ Source/Shift-Click/Classes/DetourInjector.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+//
+//
+// Source of Detourmethods: https://github.com/Zhentar/BetterPathfinding/blob/bpmx/BetterPathfinding/RegionPathCostHeuristic.cs
+// Credits go to Zhentar and RawCode
+//
+//
+
+namespace ShiftClick
+{
+    [StaticConstructorOnStartup]
+
+    internal static class DetourInjector
+    {
+        private static Assembly Assembly => Assembly.GetAssembly(typeof(DetourInjector));
+
+        private static string AssemblyName => Assembly.FullName.Split(',').First();
+
+        private static bool DoInject()
+        {
+            String targetMethod = "DrawConfigInterface";
+            String replaceMethod = "DrawConfigInterface";
+
+            MethodInfo RimWorld_Bill_Production_Draw = typeof(RimWorld.Bill_Production).GetMethod(targetMethod, UniversalBindingFlags); // new [] { typeof(Rect), typeof(Color) }
+            MethodInfo ModTest_Bill_Production_Draw = typeof(Bill_ProductionMod).GetMethod(replaceMethod, UniversalBindingFlags);
+
+            if (RimWorld_Bill_Production_Draw == null)
+                Log.Error("Fuck");
+
+            if (!Detours.TryDetourFromTo(RimWorld_Bill_Production_Draw, ModTest_Bill_Production_Draw))
+                return false;
+
+            return true;
+        }
+
+        static DetourInjector()
+        {
+            LongEventHandler.QueueLongEvent(Inject, "Initializing", true, null);
+
+#if DEBUG
+
+			if (Prefs.DevMode)
+			{
+				DebugViewSettings.drawPaths = true;
+			}
+
+#endif
+
+        }
+
+        private static void Inject()
+        {
+            if (DoInject())
+                Log.Message(AssemblyName + " injected.");
+            else
+                Log.Error(AssemblyName + " failed to get injected properly.");
+        }
+
+        private const BindingFlags UniversalBindingFlags = BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic;
+
+        private static bool DoDetour(Type rimworld, Type mod, string method)
+        {
+            MethodInfo RimWorld_A = rimworld.GetMethod(method, UniversalBindingFlags);
+            MethodInfo ModTest_A = mod.GetMethod(method, UniversalBindingFlags);
+            if (!Detours.TryDetourFromTo(RimWorld_A, ModTest_A))
+                return false;
+            return true;
+        }
+
+    }
+}
+
---
+++ readme.txt
@@ -0,0 +1,40 @@
+		+--------------------------+
+		|       Shift - Click      |
+		+--------------------------+
+
++--------------------+
+|       Author       |
++--------------------+
+
+	Proxava
+
+
++--------------------+
+| How to install it: |
++--------------------+
+
+	1. Go to your Rimwolrd directory.
+	2. Unzip the ShiftClick folder into the Mods folder.
+	3. Activate the mod in the Rimworld menu.
+
+
++--------------------+
+|      Credits       |
++--------------------+
+
+	RawCode for Detouring
+
+
+
++--------------------+
+|      License       |
++--------------------+
+
+	Free Public License 1.0.0
+
+	Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+	THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED 
+	WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR 
+	CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+	NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
