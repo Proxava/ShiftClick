6af763d5b90bb12dff623dfa166062be707e9683
Proxava <proxava@gmail.com>
2017-01-04 16:53:16 +0100

    Update 1.2
    Resolved a graphical issue and added a 25 changing control-click.

--- Source/Shift-Click/Classes/Bill_ProductionMod.cs
+++ Source/Shift-Click/Classes/Bill_ProductionMod.cs
@@ -16,149 +16,131 @@ namespace ShiftClick
         /*
         public BillRepeatMode repeatMode;
 
-        public int repeatCount = 1;
+		public int repeatCount = 1;
 
-        public int targetCount = 10;
+		public int targetCount = 10;
 
-        public BillStoreMode storeMode = BillStoreMode.BestStockpile;
+		public BillStoreMode storeMode = BillStoreMode.BestStockpile;
 
-        public string RepeatInfoText
-        {
-            get
-            {
-                if (this.repeatMode == BillRepeatMode.Forever)
-                {
-                    return "Forever".Translate();
-                }
-                if (this.repeatMode == BillRepeatMode.RepeatCount)
-                {
-                    return this.repeatCount.ToString() + "x";
-                }
-                if (this.repeatMode == BillRepeatMode.TargetCount)
-                {
-                    return this.recipe.WorkerCounter.CountProducts((Bill_Production)this).ToString() + "/" + this.targetCount.ToString();
-                }
-                throw new InvalidOperationException();
-            }
-        }
+		public string RepeatInfoText
+		{
+			get
+			{
+				if (this.repeatMode == BillRepeatMode.Forever)
+				{
+					return "Forever".Translate();
+				}
+				if (this.repeatMode == BillRepeatMode.RepeatCount)
+				{
+					return this.repeatCount.ToString() + "x";
+				}
+				if (this.repeatMode == BillRepeatMode.TargetCount)
+				{
+					return this.recipe.WorkerCounter.CountProducts(this).ToString() + "/" + this.targetCount.ToString();
+				}
+				throw new InvalidOperationException();
+			}
+		}
 
-        public static explicit operator Bill_Production(Bill_ProductionMod v)
-        {
-            throw new NotImplementedException();
-        }
+		public Bill_Production()
+		{
+		}
 
-        public Bill_ProductionMod()
-        {
-        }
+		public Bill_Production(RecipeDef recipe) : base(recipe)
+		{
+		}
 
-        public Bill_ProductionMod(RecipeDef recipe) : base(recipe)
-        {
-        }
+		public override void ExposeData()
+		{
+			base.ExposeData();
+			Scribe_Values.LookValue<int>(ref this.repeatCount, "repeatCount", 0, false);
+			Scribe_Values.LookValue<int>(ref this.targetCount, "targetCount", 0, false);
+			Scribe_Values.LookValue<BillRepeatMode>(ref this.repeatMode, "repeatMode", BillRepeatMode.RepeatCount, false);
+			Scribe_Values.LookValue<BillStoreMode>(ref this.storeMode, "storeMode", BillStoreMode.DropOnFloor, false);
+		}
 
-        public override void ExposeData()
-        {
-            base.ExposeData();
-            Scribe_Values.LookValue<int>(ref this.repeatCount, "repeatCount", 0, false);
-            Scribe_Values.LookValue<int>(ref this.targetCount, "targetCount", 0, false);
-            Scribe_Values.LookValue<BillRepeatMode>(ref this.repeatMode, "repeatMode", BillRepeatMode.RepeatCount, false);
-            Scribe_Values.LookValue<BillStoreMode>(ref this.storeMode, "storeMode", BillStoreMode.DropOnFloor, false);
-        }
+		public override BillStoreMode GetStoreMode()
+		{
+			return this.storeMode;
+		}
 
-        public override BillStoreMode GetStoreMode()
-        {
-            return this.storeMode;
-        }
+		public override bool ShouldDoNow()
+		{
+			if (this.suspended)
+			{
+				return false;
+			}
+			if (this.repeatMode == BillRepeatMode.Forever)
+			{
+				return true;
+			}
+			if (this.repeatMode == BillRepeatMode.RepeatCount)
+			{
+				return this.repeatCount > 0;
+			}
+			if (this.repeatMode == BillRepeatMode.TargetCount)
+			{
+				return this.recipe.WorkerCounter.CountProducts(this) < this.targetCount;
+			}
+			throw new InvalidOperationException();
+		}
 
-        public override bool ShouldDoNow()
-        {
-            if (this.suspended)
-            {
-                return false;
-            }
-            if (this.repeatMode == BillRepeatMode.Forever)
-            {
-                return true;
-            }
-            if (this.repeatMode == BillRepeatMode.RepeatCount)
-            {
-                return this.repeatCount > 0;
-            }
-            if (this.repeatMode == BillRepeatMode.TargetCount)
-            {
-                return this.recipe.WorkerCounter.CountProducts((Bill_Production)this) < this.targetCount;
-            }
-            throw new InvalidOperationException();
-        }
-
-        public override void Notify_IterationCompleted(Pawn billDoer, List<Thing> ingredients)
-        {
-            base.Notify_IterationCompleted(billDoer, ingredients);
-            if (this.repeatMode == BillRepeatMode.RepeatCount)
-            {
-                this.repeatCount--;
-                if (this.repeatCount == 0)
-                {
-                    Messages.Message("MessageBillComplete".Translate(new object[]
-                    {
-                        this.LabelCap
-                    }), (Thing)this.billStack.billGiver, MessageSound.Benefit);
-                }
-            }
-        }
+		public override void Notify_IterationCompleted(Pawn billDoer, List<Thing> ingredients)
+		{
+			if (this.repeatMode == BillRepeatMode.RepeatCount)
+			{
+				this.repeatCount--;
+				if (this.repeatCount == 0)
+				{
+					Messages.Message("MessageBillComplete".Translate(new object[]
+					{
+						this.LabelCap
+					}), (Thing)this.billStack.billGiver, MessageSound.Benefit);
+				}
+			}
+		}
         */
 
         protected override void DoConfigInterface(Rect baseRect, Color baseColor)
         {
-            Event e = Event.current;  // in order to catch a shift-click
             Rect rect = new Rect(28f, 32f, 100f, 30f);
             GUI.color = new Color(1f, 1f, 1f, 0.65f);
             Widgets.Label(rect, this.RepeatInfoText);
             GUI.color = baseColor;
             WidgetRow widgetRow = new WidgetRow(baseRect.xMax, baseRect.y + 29f, UIDirection.LeftThenUp, 99999f, 4f);
+
             if (widgetRow.ButtonText("Details".Translate() + "...", null, true, false))
             {
                 Find.WindowStack.Add(new Dialog_BillConfig(this, ((Thing)this.billStack.billGiver).Position));
             }
+
             if (widgetRow.ButtonText(this.repeatMode.GetLabel().PadRight(20), null, true, false))
             {
                 BillRepeatModeUtility.MakeConfigFloatMenu(this);
             }
 
-            int shift = 5;  // Shift Increment
-            // Modified Minus Button with shift ability
+            Event e = Event.current;
+            int modifier = 1;
+
+            if (e.shift)
+                modifier = 5;
+            else if (e.control)
+                modifier = 25;
+
             if (widgetRow.ButtonIcon(TexButton.Plus, null))
             {
-                if (e.shift)
+                if (this.repeatMode == BillRepeatMode.Forever)
                 {
-                    if (this.repeatMode == BillRepeatMode.Forever)
-                    {
-                        this.repeatMode = BillRepeatMode.RepeatCount;
-                        this.repeatCount = shift;
-                    }
-                    else if (this.repeatMode == BillRepeatMode.TargetCount)
-                    {
-                        this.targetCount += this.recipe.targetCountAdjustment - 1 + shift;
-                    }
-                    else if (this.repeatMode == BillRepeatMode.RepeatCount)
-                    {
-                        this.repeatCount += shift;
-                    }
+                    this.repeatMode = BillRepeatMode.RepeatCount;
+                    this.repeatCount = modifier;
                 }
-                else
+                else if (this.repeatMode == BillRepeatMode.TargetCount)
                 {
-                    if (this.repeatMode == BillRepeatMode.Forever)
-                    {
-                        this.repeatMode = BillRepeatMode.RepeatCount;
-                        this.repeatCount = 1;
-                    }
-                    else if (this.repeatMode == BillRepeatMode.TargetCount)
-                    {
-                        this.targetCount += this.recipe.targetCountAdjustment;
-                    }
-                    else if (this.repeatMode == BillRepeatMode.RepeatCount)
-                    {
-                        this.repeatCount++;
-                    }
+                    this.targetCount += modifier;
+                }
+                else if (this.repeatMode == BillRepeatMode.RepeatCount)
+                {
+                    this.repeatCount += modifier;
                 }
                 SoundDefOf.AmountIncrement.PlayOneShotOnCamera();
                 if (TutorSystem.TutorialMode && this.repeatMode == BillRepeatMode.RepeatCount)
@@ -166,40 +148,20 @@ namespace ShiftClick
                     TutorSystem.Notify_Event(this.recipe.defName + "-RepeatCountSetTo-" + this.repeatCount);
                 }
             }
-            // Modified Minus Button with shift ability
             if (widgetRow.ButtonIcon(TexButton.Minus, null))
             {
-                if (e.shift)
+                if (this.repeatMode == BillRepeatMode.Forever)
                 {
-                    if (this.repeatMode == BillRepeatMode.Forever)
-                    {
-                        this.repeatMode = BillRepeatMode.RepeatCount;
-                        this.repeatCount = 1;
-                    }
-                    else if (this.repeatMode == BillRepeatMode.TargetCount)
-                    {
-                        this.targetCount = Mathf.Max(0, this.targetCount - this.recipe.targetCountAdjustment + 1 - shift);
-                    }
-                    else if (this.repeatMode == BillRepeatMode.RepeatCount)
-                    {
-                        this.repeatCount = Mathf.Max(0, this.repeatCount - shift);
-                    }
+                    this.repeatMode = BillRepeatMode.RepeatCount;
+                    this.repeatCount = modifier;
                 }
-                else
+                else if (this.repeatMode == BillRepeatMode.TargetCount)
                 {
-                    if (this.repeatMode == BillRepeatMode.Forever)
-                    {
-                        this.repeatMode = BillRepeatMode.RepeatCount;
-                        this.repeatCount = 1;
-                    }
-                    else if (this.repeatMode == BillRepeatMode.TargetCount)
-                    {
-                        this.targetCount = Mathf.Max(0, this.targetCount - this.recipe.targetCountAdjustment);
-                    }
-                    else if (this.repeatMode == BillRepeatMode.RepeatCount)
-                    {
-                        this.repeatCount = Mathf.Max(0, this.repeatCount - 1);
-                    }
+                    this.targetCount = Mathf.Max(0, this.targetCount - modifier);
+                }
+                else if (this.repeatMode == BillRepeatMode.RepeatCount)
+                {
+                    this.repeatCount = Mathf.Max(0, this.repeatCount - modifier);
                 }
                 SoundDefOf.AmountDecrement.PlayOneShotOnCamera();
                 if (TutorSystem.TutorialMode && this.repeatMode == BillRepeatMode.RepeatCount)
